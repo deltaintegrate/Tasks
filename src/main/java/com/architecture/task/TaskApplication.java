9765fec71048e8262fab76af8baf50fe285c0607
Leonardo <leonardocastrillongiraldo@gmail.com>
2024-04-08 14:31:06 -0500

    Initial Commit

---
+++ src/main/java/com/architecture/task/TaskApplication.java
@@ -0,0 +1,13 @@
+package com.architecture.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApplication.class, args);
+	}
+
+}
